8169299c05e65072ecb6cf0c623e57f00089adb6
marg011 <margo171190@mail.ru>
2025-08-02 18:16:15 +0400

    corrected delete method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,37 +5,36 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int arraySize = 0;
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, arraySize, null);
-        arraySize = 0;
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
-        storage[arraySize] = r;
-        arraySize++;
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        if (arraySize > 0) {
-            for (int i = 0; i < arraySize; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    return storage[i];
-                }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        if (arraySize > 0) {
-            for (int i = 0; i < arraySize; i++) {
-                if (storage[i].getUuid().equals(uuid)) {
-                    System.arraycopy(storage, i + 1, storage, i, arraySize - i);
-                    arraySize--;
-                    break;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                if (i == size - 1) {
+                    storage[i] = null;
+                } else {
+                    System.arraycopy(storage, i + 1, storage, i, size - i);
+                    size--;
                 }
+                break;
             }
         }
     }
@@ -44,10 +43,10 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, arraySize);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return arraySize;
+        return size;
     }
 }
